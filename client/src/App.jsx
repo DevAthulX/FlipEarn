b63bc8047a50e1367028ee19654e9167387e12fc
DevAthulX <athul.cs.dev@gmail.com>
2026-01-01 12:39:36 +0530

    admin panel and server init (express)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,15 @@ import Loading from './pages/Loading'
 import {useLocation} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import ChatBox from "./components/ChatBox"
+import Layout from "./pages/admin/Layout"
+import Dashboard from "./pages/admin/Dashboard"
+import AllListings from './pages/admin/AllListings'
+import CredentialChange from './pages/admin/CredentialChange'
+import CredentialVerify from './pages/admin/CredentialVerify'
+import Transactions from './pages/admin/Transactions'
+import Withdrawal  from './pages/admin/Withdrawal'
+
+im
 const App = () => {
   const {pathname}= useLocation()
   return (
@@ -25,6 +34,14 @@ const App = () => {
         <Route path="/messages" element={<Messages />}/>
         <Route path="/my-orders" element={<MyOrders />}/>
         <Route path="/loading" element={<Loading />}/>
+        <Route  path="/admin" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="verify-credentials" element={<CredentialVerify />} />
+        <Route path="change-credentials" element={<CredentialChange />} />
+        <Route path="list-listings" element={<AllListings />} />
+        <Route path="transactions" element={<Transactions />} />
+        <Route path="waithdrawal" element={<Withdrawal />} />
+        </Route>
       </Routes>
       <ChatBox />
     </div>
--- client/src/pages/MyListing.jsx
+++ client/src/pages/MyListing.jsx
@@ -1,7 +1,33 @@
+import { Eye, Plus } from 'lucide-react'
+import {useSelector} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import StatCard from '../components/StatCard'
 const MyListing = () => {
+  const {useListings , balance} =useSelector((state) =>state.listing)
+  const currency =import.meta.env.VITE_CURRENCY 
+  const navigate = useNavigate()
+
+  const totalValue = userListing.reduce((sum, listing)=>sum + (listing.price|| 0),0 );
+  const activeListings = useListings.filter((listing) => listing.status === 'actilve').length;
+  const soldListingd = useListings.filter((listing) => listing.status === 'sold').length;
   return (
-    <div>
-        <h1>Listings page</h1>
+    <div className='px-6 md:px-16 lg:px-24 xl:px-32 p-8'>
+      {/* Header */}
+      <div className='flex flex-col md:flex-row justify-between item-start md:items-center mb-8'>
+        <div cla>
+          <h1 className='text-3xl font-bold text-gray-800'>My Listings</h1>
+          <p className='text-gray-600 mt-1 '>Manage your social media account listings</p>
+        </div>
+        <button onClick={()=> navigate('/create-listing')} className='bg-indigo-600 hover:bg-indigo-700 text-white px-6 py-2 rounded font-medium flex items-center space-x-2 mt-4 md:mt-0'>
+          <Plus className='size-4 '/>
+          <span>New Listing</span>
+        </button>
+      </div>
+      {/* Stats */}
+      <div className='grid grid-cols-1 md:grid-cols-4 gap-6 mb-8'>
+        <StatCard title='Total Listings' value={userListing.length} icon={<Eye className='size-6 text-indigo-600'/>}/>
+      </div>
+       
     </div>
   )
 }
